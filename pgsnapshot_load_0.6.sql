e80cf51ca66ac32774ad47bd0a1dee48d6218684
Christoph Haag <haagch.christoph@googlemail.com>
2012-12-09 18:31:05 +0100

    add highway_* table creation that dbstreets.py uses

--- pgsnapshot_load_0.6.sql
+++ pgsnapshot_load_0.6.sql
@@ -26,4 +26,24 @@ CREATE INDEX idx_nodes_geom ON nodes USING gist (geom);
 CREATE INDEX idx_way_nodes_node_id ON way_nodes USING btree (node_id);
 CREATE INDEX idx_relation_members_member_id_and_type ON relation_members USING btree (member_id, member_type);
 
+-- for dbstreets: this will select only the rows and columns relevant to the turn-by-turn navigation into highway_nodes, highway_ways and highway_way_nodes
+SELECT DISTINCT ways.id, ways.tags, ways.nodes INTO highway_ways FROM ways WHERE ways.tags::hstore ? 'highway';
+CREATE INDEX idx_highway_ways_id ON highway_ways(id);
+ALTER TABLE highway_ways ADD PRIMARY KEY(id);
+
+SELECT DISTINCT way_nodes.way_id, way_nodes.node_id, way_nodes.sequence_id INTO highway_way_nodes FROM way_nodes INNER JOIN highway_ways ON way_nodes.way_id = highway_ways.id;
+CREATE INDEX idx_highway_way_nodes_node_id ON highway_way_nodes(node_id);
+ALTER TABLE highway_way_nodes ADD PRIMARY KEY(node_id);
+ALTER TABLE highway_way_nodes ADD FOREIGN KEY(way_id) REFERENCES highway_ways(id);
+
+SELECT DISTINCT nodes.id, nodes.tags, nodes.geom INTO highway_nodes FROM nodes INNER JOIN highway_way_nodes ON nodes.id = highway_way_nodes.node_id;
+ALTER TABLE highway_nodes ADD CONSTRAINT id_unique UNIQUE (id);
+CREATE INDEX idx_highway_nodes_geom ON highway_nodes USING GIST(geom);
+ALTER TABLE highway_way_nodes ADD FOREIGN KEY(node_id) REFERENCES highway_nodes(id);
+
+GRANT ALL on highway_nodes, highway_way_nodes, highway_ways TO osm;
+-- could take a long time:
+--CLUSTER highway_nodes USING idx_highway_nodes_geom;
+-- end stuff for dbstreets
+
 VACUUM ANALYZE;
