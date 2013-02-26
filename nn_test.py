9fbfd264d93e3ca77688aa0df2ad8e45c45e5df5
Christoph Haag <haagch.christoph@googlemail.com>
2013-02-26 16:25:27 +0100

    add prepared statement option to nn_test

--- nn_test.py
+++ nn_test.py
@@ -2,12 +2,26 @@
 import psycopg2,sys,time
 
 if len(sys.argv) < 3:
-    print("nn_test.py random_coordinatefile(line = 'int_lon int_lat') csvfile")
+    print("nn_test.py random_coordinatefile(line = 'int_lon int_lat') csvfile [pp|normal]")
     exit(1)
+
+pps_create = """PREPARE nns1(text,int) AS
+SELECT id,geom
+FROM highway_nodes
+ORDER BY geom <-> ST_GeomFromEWKT($1)
+LIMIT $2;"""
+
+if len(sys.argv) == 4:
+    pp = sys.argv[3] == "pp"
+else:
+    pp = "normal"
 
 conn = psycopg2.connect("dbname=gis user=osm")
 cur = conn.cursor()
 nodes_table = "highway_nodes"
+
+if pp:
+    cur.execute(pps_create)
 
 statistic = open(sys.argv[2], 'w')
 statistic.write("number of nearest neighbours,shortest,longest,total\n")
@@ -22,7 +36,10 @@ for NN in range(1,21):
         c = cstring.split(" ")
         lon = (float(c[0])) / 10**7
         lat = (float(c[1])) / 10**7
-        query = "SELECT id,geom FROM " + nodes_table + " ORDER BY geom <#> ST_GeomFromEWKT('SRID=4326;POINT(" + str(lon) + " " + str(lat) + ")') LIMIT " + str(NN) + ";"
+        if pp:
+            query = "EXECUTE nns1('SRID=4326;POINT(" + str(lon) + " " + str(lat) + ")', " + str(NN) + ");"
+        else:
+            query = "SELECT id,geom FROM " + nodes_table + " ORDER BY geom <#> ST_GeomFromEWKT('SRID=4326;POINT(" + str(lon) + " " + str(lat) + ")') LIMIT " + str(NN) + ";"
         start = time.time()
         cur.execute(query)
         stop = time.time()
